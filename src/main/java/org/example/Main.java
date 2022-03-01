809704fca12d2e53df5f8d238a2876fab60f9a45
Ashish <juyal.ashish@gmail.com>
2022-03-01 20:35:35 +0530

    initial commit with firefox and chrome driver

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    WebDriver driver = firefoxDriver();
+
+    driver.get("http://google.com");
+
+    // document.getElementsByName("q")[0]
+    WebElement searchBox = driver.findElement(By.name("q"));
+    searchBox.click();
+    // document.getElementsByName("q")[0].value = "Selenium Testing"
+    searchBox.sendKeys("Selenium Testing");
+    searchBox.sendKeys(Keys.ENTER);
+  }
+
+  public static WebDriver firefoxDriver() {
+    return new FirefoxDriver();
+  }
+
+  public static WebDriver chromeDriver() {
+    ChromeOptions options = new ChromeOptions();
+    options.setBinary("/usr/bin/brave-browser");
+    return new ChromeDriver(options);
+  }
+
+}
