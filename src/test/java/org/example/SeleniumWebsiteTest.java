993aa6af8f5227611808b64b5e372cd66516ac0a
Ashish <juyal.ashish@gmail.com>
2022-03-04 11:28:55 +0530

    refactored test using before and beforeclass

--- src/test/java/org/example/SeleniumWebsiteTest.java
+++ src/test/java/org/example/SeleniumWebsiteTest.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,17 +14,28 @@ import static org.junit.Assert.assertEquals;
 
 public class SeleniumWebsiteTest {
 
-  @Test
-  public void test_selenium_dot_dev_blog_2016_page() {
+  private WebDriver driver;
+
+  @BeforeClass
+  public static void init() {
     WebDriverManager.firefoxdriver().setup();
+  }
 
-    WebDriver driver = new FirefoxDriver();
-
+  @Before
+  public void setup() {
+    driver = new FirefoxDriver();
     driver.get("https://selenium.dev");
-
     WebElement blog = driver.findElement(By.linkText("Blog"));
     blog.click();
+  }
 
+  @After
+  public void closeBrowser() {
+    driver.quit();
+  }
+
+  @Test
+  public void test_selenium_dot_dev_blog_2016_page() {
     WebElement link2016 = driver.findElement(By.id("m-blog2016"));
     link2016.click();
 
@@ -30,28 +44,15 @@ public class SeleniumWebsiteTest {
 
     String title = driver.getTitle();
     assertEquals("Blog Posts - 2016 | Selenium", title);
-
-    driver.quit();
   }
 
   @Test
   public void test_selenium_dot_dev_blog_2017_page() {
-    WebDriverManager.firefoxdriver().setup();
-
-    WebDriver driver = new FirefoxDriver();
-
-    driver.get("https://selenium.dev");
-
-    WebElement blog = driver.findElement(By.linkText("Blog"));
-    blog.click();
-
     WebElement link2017 = driver.findElement(By.id("m-blog2017"));
     link2017.click();
 
     String title = driver.getTitle();
     assertEquals("Blog Posts - 2017 | Selenium", title);
-
-    driver.quit();
   }
 
 }
